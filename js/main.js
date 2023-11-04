7b897a17606e72ffd65afbc128b1d1ffd190a68c
Кристина280796 <chetvergova_kristina@mail.ru>
2023-11-04 16:19:01 +0300

    add: много ошибок

--- js/main.js
+++ js/main.js
@@ -35,3 +35,44 @@ mMenuToggle.addEventListener("click", (event) => {
   event.preventDefault();
   menu.classList.contains("is-open") ? closeMenu() : openMenu(); 
 });
+
+
+
+const swiper = new Swiper('.swiper', {
+  speed: 400,
+  autoHeight: true,
+  slidesPerView: 1,
+    navigation: {
+    nextEl: ".slider-button-next",
+    prevEl: ".slider-button-prev",
+    prevE1: ".slider-buttons-prev-steps",
+    nextE1: ".slider-buttons-next-steps",
+    },
+    breakpoints: {
+      // when window width is >= 320px
+      576: {
+        slidesPerView: 2,
+        centeredSlides:true,
+        slidesOffsetBefore:-100,
+        slidesOffsetAfter:100,
+      },
+      // when window width is >= 480px
+      768: {
+        slidesPerView: 3,
+      },
+      // when window width is >= 640px
+      1024: {
+        slidesPerView: 4,
+      },
+      // when window width is >= 1200px
+      1200: {
+        slidesPerView: 5,
+      },
+    },
+  });
+
+  
+
+  
+
+  
